b9d8a8a747293c207239c5ed96d969d9ba590775
Toti Muñoz <totigm@gmail.com>
2022-08-30 07:00:00 -0300

    ✨ Add sticker command

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,23 @@ import WhatsappBot from "@totigm/whatsapp-bot";
 
 const bot = new WhatsappBot();
 
-bot.addCommand("hello", () => "world!");
+bot.addCommand(
+    "sticker",
+    async (message) => {
+        if (message.hasMedia) {
+            const media = await message.downloadMedia();
+            if (media.data)
+                message.reply(media, message.from, {
+                    sendMediaAsSticker: true,
+                });
+        } else message.reply("Send an image or video to convert it to a sticker");
+    },
+    {
+        description: "Convert an image or video to a sticker",
+        explanation: "Send an image or video with the text !sticker and you will get it as an sticker",
+        example: {
+            input: "image/video",
+            output: "sticker",
+        },
+    },
+);
